af919e76082a9d4f1fa33e7820a5d22a6d0094cb
mdttrump97 <mdttrump97@bellsouth.net>
2016-03-19 11:04:23 -0500

    Adds subsystem class for Churro Lock servo

---
+++ ChurroLock.java
@@ -0,0 +1,44 @@
+package com.FTC3486.Subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Matthew on 1/26/2016.
+ */
+public class ChurroLock {
+    Servo churroLock;
+
+    enum churroLockEnumeration {UNLOCKED, LOCKED}
+
+    churroLockEnumeration churroLockState = churroLockEnumeration.UNLOCKED;
+
+    public ChurroLock(String churroLock, HardwareMap hardwareMap) {
+        this.churroLock = hardwareMap.servo.get(churroLock);
+        this.unlock();
+    }
+
+    public void unlock() {
+        churroLock.setPosition(1);
+        churroLockState = churroLockEnumeration.UNLOCKED;
+    }
+
+    public void lock() {
+        churroLock.setPosition(0.0);
+        churroLockState = churroLockEnumeration.LOCKED;
+    }
+
+    @Override
+    public String toString() {
+        switch (churroLockState) {
+            case UNLOCKED:
+                return "UNLOCKED";
+
+            case LOCKED:
+                return "LOCKED";
+
+            default:
+                return "UNKNOWN";
+        }
+    }
+}
